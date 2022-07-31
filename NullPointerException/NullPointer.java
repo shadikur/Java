45252c1333897800884bbeb1cb3b310f1b76fa5e
Shadikur <me@shadikur.com>
2022-07-31 17:20:50 +0200

    Java Null Pointer Exeption

---
+++ NullPointerException/NullPointer.java
@@ -0,0 +1,12 @@
+package NullPointerException;
+
+public class NullPointer {
+    public static void main(String[] args) {
+        String s = "MicrotechNA";
+        if("Skyit".equals(s)) {
+            System.out.println("Both string are same");
+        } else {
+            System.out.println("Not same");
+        }
+    }
+}
